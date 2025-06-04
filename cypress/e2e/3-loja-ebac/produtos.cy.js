823ffbc030151c5d320b53a311b5cc890810057b
Tatiana de Oliveira <tatiana_any@hotmail.com>
2025-06-04 20:44:40 -0300

    faker e variáveis

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('https://lojaebac.ebaconline.art.br/produtos')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+    
+        
+    });
+});
